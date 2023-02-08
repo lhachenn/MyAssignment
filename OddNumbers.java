4b1b00b818806dbaf4a947fec0776bdf14e563cf
lhachenn <124641790+lhachenn@users.noreply.github.com>
2023-02-08 11:26:35 -0500

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,10 @@
+public class OddNumbers {
+    public static void main(String[] args) {
+        int n=20;
+        for (int i=2; i<=n; i++)
+        {
+            if (i%2==1)
+                System.out.println("Odd Numbers are:"+i);
+        }
+    }
+}
