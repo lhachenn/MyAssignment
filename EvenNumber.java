82886597dead3f2020038ab8ef4ca25498fde336
lhachenn <124641790+lhachenn@users.noreply.github.com>
2023-02-08 10:15:40 -0500

    Add files via upload

---
+++ EvenNumber.java
@@ -0,0 +1,10 @@
+public class EvenNumber {
+    public static void main(String[] args) {
+        for (int i=3; i<=30; i++)
+        {
+            if (i%2==0)
+                System.out.println("Even Numbers are:"+i);
+            }
+        }
+    }
+
